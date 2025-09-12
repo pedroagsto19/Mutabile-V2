5a644be9b329cf2b1fa0be1bccad2843737eef62
pedroagsto19 <pedroaomatos@hotmail.com>
2025-09-12 13:02:22 -0300

    Updated SupplierForm.tsx

--- src/components/Suppliers/SupplierForm.tsx
+++ src/components/Suppliers/SupplierForm.tsx
@@ -532,6 +532,7 @@ export function SupplierForm({ isOpen, onClose, supplier }: SupplierFormProps) {
                   </>
                 )}
               </div>
+            </div>
             
             <div>
               <label className="block text-sm font-medium text-gray-700 mb-2">
